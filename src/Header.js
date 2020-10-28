43de9364f2f9b8fab4408a7bb99825784e5fd012
Jena Boehm <boehmjena@gmail.com>
2020-10-28 15:36:24 -0700

    Add CSS

--- src/Header.js
+++ src/Header.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react'
 export default class Header extends Component {
     render() {
         return (
-            <div>
-                <header>Pokedex</header>
-                <div>Navigation</div>
+            <div className="header">
+                <div className="header-title">Pokedex</div>
+                <div className="navigation">Navigation</div>
             </div>
         )
     }
--- src/PokeItem.js
+++ src/PokeItem.js
@@ -4,7 +4,7 @@ import data from './data.js';
 export default class PokeItem extends Component {
     render() {
         return (
-            <div className='image-item'>
+            <div className='poke-item'>
                 <img className='image' src={this.props.image} alt={this.props.name} />
                 <div className='name'>{this.props.name}</div>
                 <div className='type'>{this.props.type}</div>
